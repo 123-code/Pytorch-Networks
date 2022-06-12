973c26bdc41368e75a15f320985fce75831b1827
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2022-06-11 22:30:30 -0500

    Updated Net

--- include/Digitrecognizer.py
+++ include/Digitrecognizer.py
@@ -29,10 +29,35 @@ for i in train_dataloader:
 # creacion de clase de la red neuronal 
 class NN(nn.Module):
     def __init__(self):
-        super.__init()
+        super(NN,self).__init__()
+        self.flatten = nn.Flatten()
+        # creating network layers
+        self.l1 = torch.nn.Linear(784, 256)
+        self.l2 = torch.nn.Linear(256,10)
+        # setting activation functions up
+        self.activation = torch.nn.ReLU()
+        self.Softmax = torch.nn.Softmax()
 
+# Model's Forward operations.
+    def forward(self,x_batch):
+        x_batch = self.l1(x_batch)
+        x_batch = self.activation(x_batch)
+        x_batch = self.l2(x_batch)
+        x_batch = self.Softmax(x_batch)
+        return x_batch
 
+ 
+digitrecognizer = NN()
 
+print(digitrecognizer)
+
+        
+numbers = torch.randn(1,28,28)
+#print(numbers)
+Forward_pass = digitrecognizer.forward(numbers)
+probability = nn.Softmax(dim=1)
+y_pred = probability.argmax(1)
+print(f"Predicted:{y_pred}")
 print("Funcionó")
 
 '''
