c70d704c99c7e6a57575002cb26fbff33297aeab
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2023-05-27 05:33:09 +0000

    Kaggle API fixes & tokenizing

--- include/SentimentClassifier.py
+++ include/SentimentClassifier.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
 import kaggle 
 import pandas as pd
 import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 from transformers import BertTokenizer
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np 
+
 
 api = KaggleApi()
 api.authenticate()
@@ -11,15 +15,15 @@ api.authenticate()
 api.competition_download_file('sentiment-analysis-on-movie-reviews','test.tsv.zip',path='./Datasets')
 api.competition_download_file('sentiment-analysis-on-movie-reviews','train.tsv.zip',path='./Datasets')
 
-with zipfile.ZipFile('./Datasets/test.tsv.zip', 'r') as zip_ref:
+with zipfile.ZipFile('./Datasets/SentimentClassifier/test.tsv.zip', 'r') as zip_ref:
     zip_ref.extractall('./Datasets')
 
 
-with zipfile.ZipFile('./Datasets/train.tsv.zip', 'r') as zip_ref:
+with zipfile.ZipFile('../Datasets/SentimentClassifier/.tsv.zip', 'r') as zip_ref:
     zip_ref.extractall('./Datasets')
 
 
-df = pd.read_csv('./Datasets/train.tsv', sep='\t')
+df = pd.read_csv('./Datasets/SentimentClassifier/train.tsv', sep='\t')
 df['Sentiment'].value_counts().plot(kind='bar')
 
 seq_len = 512
@@ -27,3 +31,20 @@ num_samples = len(df)
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 tokens = tokenizer(df['Phrase'].tolist(),max_length=seq_len,truncation=True,padding='max_length',add_special_tokens=True,return_tensors='np')
+tokens.keys()
+print(tokens['attention_mask'])
+
+with open('movie-xids.npy','wb') as f:
+    np.save(f,tokens['input_ids'])
+
+with open('movie-xmask.npy','wb') as f:
+    np.save(f,tokens['attention_mask'])
+
+arr = df['Sentiment'].values
+arr.max() + 1
+
+labels = np.zeros((num_samples,arr.max() + 1))
+labels[np.arrange(num_samples),arr] = 1
+
+with open('movie-labels.npy','wb') as f:
+    np.save(f,labels)
